013d1f28c57f6656f766bb6c14a8c905025b8bbc
mansiarora <mansi@intelligrape.com>
2015-02-02 15:49:15 +0530

    Updated _Events.groovy file;

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,22 +1,4 @@
 import grails.util.Environment
-eventCreateWarStart = {warname, stagingDir ->
-
-    //println("---------- BuildAppVersionPropertiesStart ----------")
-    event("BuildAppVersionPropertiesStart", [warname, stagingDir])
-
-    Ant.propertyfile(file: "${stagingDir}/WEB-INF/classes/application.properties") {
-		Ant.antProject.properties.findAll({k,v-> k.startsWith('environment')}).each { k,v->
-            entry(key: k, value: v)
-		}
-        entry(key: 'scm.revision', value: getRevision().trim())
-        entry(key: 'scm.branch', value: getBranch().trim())
-        entry(key: 'buildtime', value: new Date())
-        entry(key: 'buildhost', value: java.net.InetAddress.getLocalHost().getHostName())
-    }
-
-    event("BuildAppVersionPropertiesEnd", [warname, stagingDir])
-}
-
 eventCompileEnd = {x ->
     if(Environment.currentEnvironment!=Environment.TEST) {
         compileSass()
@@ -42,48 +24,4 @@ def compileSass() {
     } else {
         event("StatusError", ["Problem compiling SASS ${proc.err.text}"])
     }
-}
-
-def getBranch() {
-
-    // try to get branch from Jenkins
-    def scmBranch = Ant.antProject.properties."environment.GIT_BRANCH"
-
-    // try git locally
-    if (!scmBranch) {
-        try {
-            def command = """git rev-parse --abbrev-ref HEAD"""
-            def proc = command.execute()
-            proc.waitFor()
-            if (proc.exitValue() == 0) {
-                scmBranch = proc.in.text
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e)
-        }
-    }
-
-    return scmBranch ?: 'UNKNOWN'
-}
-
-def getRevision() {
-
-    // try to get revision from Jenkins
-    def scmVersion = Ant.antProject.properties."environment.GIT_COMMIT"
-
-    // try git locally
-    if (!scmVersion) {
-        try {
-            def command = """git rev-parse HEAD"""
-            def proc = command.execute()
-            proc.waitFor()
-            if (proc.exitValue() == 0) {
-                scmVersion = proc.in.text
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e)
-        }
-    }
-
-    return scmVersion ?: 'UNKNOWN'
 }
